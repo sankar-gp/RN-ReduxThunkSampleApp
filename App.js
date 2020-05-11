1f63a471a75cfe2ed64251860d1f2372c04dae76
Sankar-gp <sankar.paramaguru@gmail.com>
2020-05-11 15:38:59 +0530

    cleanup

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
 import store from "./app/store/Store.js";
-import LoginScreen from "./app/components/LoginScreen.js";
 import Navigator from "./app/Navigation";
 
 export default class App extends Component {
